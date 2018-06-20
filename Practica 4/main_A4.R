7fa84a4360715d56868342d4c55893e5755340e1
Alvaro <apicatoste@gmail.com>
2018-06-20 06:54:39 +0200

    A4

--- Practica 4/main_A4.R
+++ Practica 4/main_A4.R
@@ -1,6 +1,17 @@
-dataset <- read.csv(file = './Fumadores.csv')
+library("rmarkdown")
 
-dataset
+df <- read.csv(file = './Fumadores.csv')
 
+levels(df$Tipo) <- list(NF = "NF", FP = "FP", NI = c("NI", "ni"), FL = c("FL", "fL"), FM = "FM", FI = "FI")
 
-print("yeah")
+resumen <- df %>% group_by(Tipo) %>% summarize(Media.Por.Tipo = mean(AE),
+                                               Numero.Personas = length(AE))
+print(resumen)
+g <- ggplot(resumen, aes(x = reorder(Tipo, -Media.Por.Tipo), y = Media.Por.Tipo)) + geom_col()
+print(g) 
+
+misboxplots <- ggplot(df, aes(x = Tipo, y = AE)) + geom_boxplot()
+print(misboxplots)
+
+# Ejercicio 2
+
